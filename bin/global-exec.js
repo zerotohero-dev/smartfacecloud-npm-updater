a829744b2c7725d00ff0af869fc64fd0c0b9460d
Serkan Serttop <serkanserttop@yahoo.com>
2016-02-04 17:48:56 +0200

    First integrated working version with smartfacloud.updater IDE plugin

--- bin/global-exec.js
+++ bin/global-exec.js
@@ -2,4 +2,5 @@
 
 var thisPackage = require('../index.js');
 var args = require('minimist')(process.argv.slice(2));
+args.workspacePath = args.workspacePath || '/home/ubuntu/workspace';
 thisPackage(args);
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const execFile = require('child_process').execFile;
 const fs = require('fs');
+const path = require('path');
 const semver = require('semver');
-const PACKAGE_VERSION = '0.0.1';
+const PACKAGE_VERSION = '0.0.2';
 
 const CURRENT_VERSION = {
 	major: semver.major(PACKAGE_VERSION),
@@ -23,21 +24,51 @@ function facade(args) {
 				if (globallyInstalledPackages === '') {
 					return console.log('no globallyInstalledPackages', globallyInstalledPackages);
 				}
+				// console.log('globallyInstalledPackages', globallyInstalledPackages);
 				const packagesThatCanBeInstalled = filterPackageSnapshotsThatCanBeUpdated(packageSnapshots);
+				if (!(packagesThatCanBeInstalled instanceof Array)) {
+					return console.log('packagesThatCanBeInstalled is not an Array');
+				} else if (packagesThatCanBeInstalled.length === 0) {
+					return console.log('packagesThatCanBeInstalled has length zero');
+				}
+				// console.log('packagesThatCanBeInstalled', packagesThatCanBeInstalled)
 				//In the future the below logic can be more complex
 				//There will be ways to update workspace via migrations
 				//And there will be options for upgrading minor or major versions
 				const snapshotKey = packagesThatCanBeInstalled.pop();
-				installSnapshot(snapshotKey, callbackAllInstalled);
+				installSnapshot(snapshotKey, globallyInstalledPackages.dependencies);
 			});
 		}
 	});	
 }
 
-function callbackAllInstalled(err, data) {
-	console.log(err, data);
-	execNpmGlobalDependenciesList(function(err, data) {
-		console.log(err, data);
+function buildListOfPackagesWithTagsToInstall(packagesToInstall) {
+	return packagesToInstall.map(item => {
+		return item.name + '@' + item.version;
+	});
+}
+
+function callbackAllInstalled(packagesToInstall, err, data) {
+	console.log('callbackAllInstalled', packagesToInstall, err, data);
+	execNpmGlobalDependenciesList(function(err, globallyInstalledPackages) {
+		console.log(err, globallyInstalledPackages);
+		if (err) {
+			return console.log('err', err);
+		}
+		packagesToInstall.forEach(elem => {
+			elem.installed = false;
+			if (globallyInstalledPackages.dependencies && 
+					globallyInstalledPackages.dependencies[elem.name] &&
+					globallyInstalledPackages.dependencies[elem.name].version &&
+					globallyInstalledPackages.dependencies[elem.name].version === elem.version) {
+				elem.installed = true;
+			}
+		});
+		process.stdout.write('smartface-cloud-updater has finished processing packagesInstalled');
+		process.stdout.write(JSON.stringify({
+			packagesInstalled: packagesToInstall
+		}));
+		process.stdout.write('smartface-cloud-updater json was sent');
 	});
 }
 
@@ -74,7 +105,7 @@ function filterPackageSnapshotsThatCanBeUpdated(packageSnapshots) {
 		if (key === 'meta') {
 			return false;
 		}
-		if (!semver.gt(key, PACKAGE_VERSION)) {
+		if (!semver.gte(key, PACKAGE_VERSION)) {
 			return false;
 		}
 		return true;
@@ -82,29 +113,50 @@ function filterPackageSnapshotsThatCanBeUpdated(packageSnapshots) {
 	return patchLevelGreaterVersionKeys.sort(semver.compare);
 }
 
-function installPackages(packageSnapshots, callbackAllInstalled) {
-	const packages = Object.keys(packageSnapshots).map(function(key) {
-		return key + '@' + packageSnapshots[key];
-	});
-	const cmd = ['install', '-g', '--json'].concat(packages).join(' ');
-	execNpmWrapper(['install', '-g', '--json'].concat(packages), callbackAllInstalled);
+function installPackages(packagesToInstall) {
+	const packages = buildListOfPackagesWithTagsToInstall(packagesToInstall);
+	execNpmWrapper(['install', '-g', '--json'].concat(packages), callbackAllInstalled.bind(null, packagesToInstall));
 }
 
-function installSnapshot(snapshotKey, callbackAllInstalled) {
-	fs.readFile('snapshots/' + CURRENT_VERSION.major + '.x/' + snapshotKey + '.json', function(err, data) {
+function installSnapshot(snapshotKey, globallyInstalledPackages) {
+	const dataFilePath = path.join(__dirname, 'snapshots', CURRENT_VERSION.major + '.x', snapshotKey + '.json');
+	fs.readFile(dataFilePath, function(err, data) {
+		if (err) {
+			return console.error(err);
+		}
 		var json;
 		try {
 			json = JSON.parse(data.toString());
 		} catch (e) {
-			console.log('installSnapshot no json file');
+			console.log('installSnapshot no json file', e);
 			return;
 		}
-		installPackages(json, callbackAllInstalled);
+		const packagesToInstall = filterUninstalledSnapshotPackages(globallyInstalledPackages, json);
+		// return console.log('packagesToInstall', packagesToInstall);
+		installPackages(packagesToInstall);
+	});
+}
+
+function filterUninstalledSnapshotPackages(globallyInstalledPackages, snapshotPackages) {
+	return Object.keys(snapshotPackages).filter(key => {
+		if (!globallyInstalledPackages[key]) {
+			return true;
+		}
+		if (semver.lt(globallyInstalledPackages[key].version, snapshotPackages[key])) {
+			return true;
+		}
+		return false;
+	}).map(key => {
+		return {
+			name: key,
+			version: snapshotPackages[key]
+		};
 	});
 }
 
 function loadDataFile(callback) {
-	fs.readFile('data.json', function(err, data) {
+	const dataFilePath = path.join(__dirname, 'data.json');
+	fs.readFile(dataFilePath, function(err, data) {
 	  if (err) {
 	  	return callback(err);
 	  }
